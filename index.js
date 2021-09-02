e2508bc8064cb784ffff85e1479df393aac4ccc3
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-09-02 09:39:33 +0300

    fix after code review

--- index.js
+++ index.js
@@ -489,6 +489,10 @@ function copyStaticDocs ({source, outputTo: outputBase, icon}) {
 function extractLibraryDescription ({path, hasPackageDir, description, ...rest}, strict) {
 	const output = {};
 	let libraryPaths;
+	
+	if(os.platform() === 'win32') {
+		path = path.replace("/", '\\');
+	}
 
 	if (hasPackageDir) {
 		const packageDir = pathModule.join(path, 'packages'),
