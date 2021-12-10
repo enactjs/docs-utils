36d320fdb44543e3adba709cfd194fd65ec0e043
taeyoung.hong <taeyoung.hong@lge.com>
2021-12-10 15:25:58 +0900

    fix severity vulnerabilities 3

--- index.js
+++ index.js
@@ -607,7 +607,7 @@ function generateIndex (docIndexFile) {
 
 	console.log('Generating search index...');	// eslint-disable-line no-console
 
-	readdirp({root: 'src/pages/docs/modules', fileFilter: '*.json'}, (err, res) => {
+	readdirp('src/pages/docs/modules', {fileFilter: '*.json'}, (err, res) => {
 		if (!err) {
 			res.files.forEach(result => {
 				const filename = result.fullPath;
