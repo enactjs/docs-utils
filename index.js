64f15f27ac78ce762f483b3e598665e099f916cb
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-09-05 17:13:49 +0300

    add alwaysTrue prop

--- index.js
+++ index.js
@@ -607,7 +607,7 @@ function generateIndex (docIndexFile) {
 
 	console.log('Generating search index...');	// eslint-disable-line no-console
 
-	readdirp('src/pages/docs/modules', {fileFilter: '*.json'}, (err, res) => {
+	readdirp('src/pages/docs/modules', {fileFilter: '*.json', alwaysStat: true}, (err, res) => {
 		if (!err) {
 			res.files.forEach(result => {
 				const filename = result.fullPath;
@@ -629,7 +629,7 @@ function generateIndex (docIndexFile) {
 			process.exit(2);
 		}
 
-		readdirp('src/pages/', {fileFilter: '*.md'}, (_err, _res) => {
+		readdirp('src/pages/', {fileFilter: '*.md', alwaysStat: true}, (_err, _res) => {
 			if (!_err) {
 				_res.files.forEach(result => {
 					const filename = result.fullPath;
